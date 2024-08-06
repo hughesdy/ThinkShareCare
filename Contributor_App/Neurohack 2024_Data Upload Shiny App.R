0999e565954ea517ae71ac63d81d947c2a9bc07f
Sarah Zapetis <sarahzapetis@gmail.com>
2024-08-06 15:14:27 -0700

    Adding first version of contributer app

--- Contributor_App/Neurohack 2024_Data Upload Shiny App.R
+++ Contributor_App/Neurohack 2024_Data Upload Shiny App.R
@@ -5,7 +5,7 @@ library(shiny)
 library(shinyjs)
 library(shinyWidgets)
 
-setwd("~/Desktop/Neurohack/Project/App")
+setwd("~/Documents/github")
 
 # Define the list of possible variables
 possible_vars <- c("Age", "Sex", "Gender", "SES", "Education", "Income", "GAD-7", "PHQ-9")
