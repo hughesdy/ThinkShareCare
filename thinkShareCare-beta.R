5554ca416d1536c5c8dbdf44dce25aa3903c8ee1
Dylan Hughes <hughesdy@g.ucla.edu>
2024-08-06 15:03:38 -0700

    Updating beta version

--- thinkShareCare-beta.R
+++ thinkShareCare-beta.R
@@ -10,53 +10,93 @@
 library(shiny)
 
 ui <- fluidPage(
-  titlePanel("Choose your variables!"),
+  titlePanel("Patient characteristics"),
   sidebarLayout(
     sidebarPanel(
-      selectInput("var1")
-    )
+      fileInput("model_file", "Upload Model File", accept = c(".csv")),
+      uiOutput("variable_inputs")
+    ),
+    mainPanel(
+      verbatimTextOutput("weights"),
+      htmlOutput("result")
+    ),
   )
 )
 
-
-
-
-# Define UI for application that draws a histogram
-# ui <- fluidPage(
-# 
-#     # Application title
-#     titlePanel("Think, Share, Care"),
-# 
-#     # Sidebar with a slider input for number of bins 
-#     sidebarLayout(
-#         sidebarPanel(
-#             sliderInput("bins",
-#                         "Number of bins:",
-#                         min = 1,
-#                         max = 50,
-#                         value = 30)
-#         ),
-# 
-#         # Show a plot of the generated distribution
-#         mainPanel(
-#            plotOutput("distPlot")
-#         )
-#     )
-# )
-
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
   
-  output$ui <- renderUI({
-    if (is.null(input$input_type))
-      return()
+
+  model_data <- reactive({
+    req(input$model_file)
+    data <- read.csv(input$model_file$datapath)
+    data
+  })
+  
+  # Reactive expression to read the model file
+  model_vars <- reactive({
+    data = model_data()
+    model_vars <- as.character(data[, "var"])
+    model_vars
+  })
+  
+  model_vals <- reactive({
+    data = model_data()
+    model_data <- read.csv(input$model_file$datapath)
+    model_vals <- as.numeric(data[,"coef"])
+    model_vals
+  })
+  
+  # Display the weights
+  output$weights <- renderPrint({
+    vars <- model_vars()
+    vals = model_vals()
     
-    # Depending on input$input_type, we'll generate a different
-    # UI component and send it to the client.
-    switch(input$var1,
-           "Age" = numericInput("age", "Age",
-                                       value = 0
-           ))
+    weights <- round(vals, 3)
+    
+    names(weights) <- vars
+    weights
+  })
+    
+  
+  # Render the input fields based on the uploaded model
+  output$variable_inputs <- renderUI({
+    vars <- model_vars()
+    
+    input_list <- lapply(vars[2:length(vars)], function(var) {
+      numericInput(inputId = var, label = var, value = NULL)
+    })
+    
+    do.call(tagList, input_list)
+  })
+  
+  # Create predicted values
+  output$result <- renderUI({
+    
+    # Pull weights from linear model
+    allWeights = model_vals()
+    
+    intercept = allWeights[1] ## take intercept 'weight'/coefficient
+    weights = allWeights[2:length(allWeights)] # take weights for individual terms
+    
+    vars = model_vars()
+    
+    # take user inputs (i.e., patient characteristics)
+    pt_values = sapply(vars[2:length(vars)], function(var) input[[var]]) 
+    
+    # Only print predicted value if all of the fields have been populated
+    if (all(!is.na(pt_values))) {
+      # Compute predicted values
+      individual_terms = sum(weights*pt_values) # multiply weights by each patient value
+      predicted = intercept + individual_terms # add the intercept term to the above
+      
+      HTML(paste0("<b>Predicted value: ", round(predicted, 3), "</b>"))
+      
+    } else {
+      HTML("")
+    }
+    
+
   })
   
 }
